416b10c6a1b56f9e6b35fb709a5da2b0e9a94ecd
sborys00 <16275683+sborys00@users.noreply.github.com>
2021-03-27 12:56:36 +0100

    fix string_to_task function

--- main.c
+++ main.c
@@ -2,5 +2,7 @@
 
 int main(int argc, char* argv)
 {
+    char str[] ="12:45:test ets stdf:1";
+    struct Task task = string_to_task(str);
     return 0;
 }
--- tasks.c
+++ tasks.c
@@ -2,17 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct Task string_to_task(char* string)
+struct Task string_to_task(char string[])
 {
     struct Task task;
     const char d[2] = ":";
-    char *token;
-    token = strtok(string, d);
-    task.time.hour = atoi(token);
-    token = strtok(NULL, d);
-    task.time.minute = atoi(token);
-    token = strtok(NULL, d);
-    strcpy(task.command, token);
-    token = strtok(NULL, d);
-    task.info = token[0];
+    char * str;
+    str = strtok (string, d);
+    task.time.hour = atoi(str);
+    str = strtok (NULL, d);
+    task.time.minute = atoi(str);
+    str = strtok (NULL, d);
+    task.command = (char *)malloc(strlen(str) * (sizeof(char) + 1));
+    strcpy(task.command, str);
+    str = strtok (NULL, d);
+    task.info = atoi(str);
+    return task;
 }
