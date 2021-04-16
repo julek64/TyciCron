a0db3fc7e7f4d920b3832289ac4c6693d61a64fd
Xanthorn <59312494+Xanthorn@users.noreply.github.com>
2021-04-16 22:07:00 +0200

    Add redirect stdout/stderr to file

--- tasks.c
+++ tasks.c
@@ -135,6 +135,34 @@ int run_task(struct Task task)
     }
     args[i] = NULL;
     
+    int outputFile = open("output.txt", O_WRONLY|O_CREAT|O_APPEND, 0666);
+
+    //Convert task to string
+    size_t len = 0;
+    len = snprintf(NULL, len, "\n\n\n%d:%d:%s:%d\n\n", task.time->hour, task.time->minute, task.command, task.info);
+    char *command = calloc (1, sizeof *command * len + 1);
+    snprintf(command, len + 1, "\n\n\n%d:%d:%s:%d\n\n", task.time->hour, task.time->minute, task.command, task.info);
+    
+    write(outputFile, command, strlen(command));
+
+    if(task.info == 0)
+    {
+        close(1); //Close stdout
+        dup(outputFile);
+    }
+    if(task.info == 1)
+    {
+        close(2); //Close stderr
+        dup(outputFile);
+    }
+    else if(task.info == 2)
+    {
+        close(1); //Close stdout
+        dup(outputFile);
+        close(2); //Close stderr
+        dup(outputFile);
+    }
+
     child_pid = fork();
     if(child_pid == -1)
         return -1;
